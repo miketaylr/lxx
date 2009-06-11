40fce436ad28a7c423f80bf705c480df4d90272c
Mike Taylor <runningtowardsnothing@gmail.com>
2009-06-10 22:20:53 -0400

    adding route for example page

--- lxx/config/routes.rb
+++ lxx/config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :examples
+  map.examples 'lxx', :controller => 'examples', :action => 'lxx'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
